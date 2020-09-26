37fd0072a7634a9dd51d7610420f76341f6056af
Brian Chen <ToucheSir@users.noreply.github.com>
2020-09-25 20:42:10 -0700

    Add dev runner to enable debug

--- backend/main.py
+++ backend/main.py
@@ -10,3 +10,11 @@ from app.api import router as api_router, audit_handler
 app = FastAPI()
 app.add_middleware(GZipMiddleware)
 app.include_router(api_router, dependencies=[Depends(audit_handler)])
+
+# Dev runner
+if __name__ == "__main__":
+    try:
+        import uvicorn
+        uvicorn.run("main:app", reload=True)
+    except:
+        print("uvicorn not found")
